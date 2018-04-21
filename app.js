c23f9290cfb8cec48e24faec1649d60087cf661d
Andrew Budi Kusuma <andrew.budikusuma@gmail.com>
2018-04-21 18:11:44 +0800

    motion for bathroom

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ const board = new five.Board({
 const door = require('./sensors/door.js')
 const garage = require('./sensors/garage.js')
 const gas = require('./sensors/gas.js')
+const bath = require('./sensors/bath.js')
 // console.log('initial server')
 board.on('ready', function() {
   console.log('sensor checking:')
   door.initial('GPIO17', 'GPIO18')
   garage.initial('GPIO22', 'GPIO23')
   gas.initial('GPIO17', 'GPIO24')
+  bath.initial('GPIO16', 'GPIO25')
 })
 
 module.exports = five
